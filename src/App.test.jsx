7d3962e27e7860531d545b46791abbcdc2992786
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-26 15:16:36 -0800

    tests passing on vscode but not github

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,7 +9,7 @@ import App from './App'
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
-  name: 'Ian',
+  name: 'Kingsley',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
